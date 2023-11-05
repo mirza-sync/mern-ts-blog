469ad485108fa9738c1dd939db073cd58572613a
mirza-sync <mirzasyahmi123@gmail.com>
2023-11-05 23:22:48 +0800

    Implement loading component and user context

--- client/src/application.tsx
+++ client/src/application.tsx
@@ -1,25 +1,67 @@
+import { useEffect, useReducer, useState } from 'react';
 import { Route, RouteChildrenProps, Switch } from 'react-router-dom';
 
+import LoadingComponent from './components/LoadingComponent';
 import routes from './config/route';
+import { initialUserState, UserContextProvider, userReducer } from './contexts/user';
 
-export interface IApplictionProps {}
+const Application: React.FunctionComponent = () => {
+  const [user, userDispatch] = useReducer(userReducer, initialUserState);
+  const [loading, setLoading] = useState<boolean>(true);
 
-const Application: React.FunctionComponent<IApplictionProps> = (props) => {
+  // Used for debugging
+  const [authStage, setAuthStage] = useState('Cheking localStorage...');
+
+  useEffect(() => {
+    setTimeout(() => {
+      checkCredentials();
+    }, 1000);
+  }, []);
+
+  const checkCredentials = () => {
+    setAuthStage('Checking credentials...');
+
+    const fireToken = localStorage.getItem('fire_token');
+    if (fireToken === null) {
+      userDispatch({ type: 'logout', payload: initialUserState });
+      setAuthStage('No credentials found.');
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    } else {
+      setAuthStage('Credentials found. Validating...');
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000);
+    }
+  };
+
+  const userContextValues = {
+    userState: user,
+    userDispatch,
+  };
+
+  if (loading) {
+    return <LoadingComponent>{authStage}</LoadingComponent>;
+  }
+
   return (
-    <Switch>
-      {routes.map((route, index) => {
-        return (
-          <Route
-            key={index}
-            exact={route.exact}
-            path={route.path}
-            render={(routerProps: RouteChildrenProps<any>) => (
-              <route.component {...routerProps} />
-            )}
-          />
-        );
-      })}
-    </Switch>
+    <UserContextProvider value={userContextValues}>
+      <Switch>
+        {routes.map((route, index) => {
+          return (
+            <Route
+              key={index}
+              exact={route.exact}
+              path={route.path}
+              render={(routerProps: RouteChildrenProps<any>) => (
+                <route.component {...routerProps} />
+              )}
+            />
+          );
+        })}
+      </Switch>
+    </UserContextProvider>
   );
 };
 
