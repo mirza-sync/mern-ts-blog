124ca8aeddae108beb034980a6c2b5259d87f218
mirza-sync <mirzasyahmi123@gmail.com>
2023-11-06 23:52:07 +0800

    Fix signInWithSocialMedia didn't return a promise

--- client/src/modules/auth.ts
+++ client/src/modules/auth.ts
@@ -3,7 +3,7 @@ import { signInWithPopup, UserCredential } from 'firebase/auth';
 import { auth, Providers } from '../config/firebase';
 
 export const signInWithSocialMedia = () => {
-  new Promise<UserCredential>((resolve, reject) => {
+  return new Promise<UserCredential>((resolve, reject) => {
     signInWithPopup(auth, Providers.google)
       .then((result) => resolve(result))
       .catch((error) => reject(error));
