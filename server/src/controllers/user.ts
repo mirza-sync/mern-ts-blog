c034572f7c45a0265e7a70fa07c2a5ad96fe082b
mirza-sync <mirzasyahmi123@gmail.com>
2023-11-09 00:02:23 +0800

    Add create/register user api

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import logging from '../config/logging';
 import User from '../models/user';
+import mongoose from 'mongoose';
 
 const validate = (req: Request, res: Response, next: NextFunction) => {
   logging.info('Token validated. Returning user...');
@@ -27,7 +28,34 @@ const validate = (req: Request, res: Response, next: NextFunction) => {
     });
 };
 
-const create = (req: Request, res: Response, next: NextFunction) => {};
+const create = (req: Request, res: Response, next: NextFunction) => {
+  logging.info('Attempting to register new user');
+
+  const { uid, name } = req.body;
+  const fireToken = res.locals.fireToken;
+
+  const user = new User({
+    _id: new mongoose.Types.ObjectId(),
+    uid,
+    name,
+  });
+
+  return user
+    .save()
+    .then((newUser) => {
+      logging.info(`User ${uid} created`);
+      return res.status(200).json({
+        user: newUser,
+        fireToken,
+      });
+    })
+    .catch((error) => {
+      logging.error(error);
+      return res.status(500).json({
+        error,
+      });
+    });
+};
 
 const login = (req: Request, res: Response, next: NextFunction) => {};
 
