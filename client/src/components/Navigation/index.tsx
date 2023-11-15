da185ea9fcdfe6715bbc1fdebd6d3d54a0f860c3
mirza-sync <mirzasyahmi123@gmail.com>
2023-11-16 00:17:39 +0800

    - Implement logout function. - Show auth buttons in navbar.

--- client/src/components/Navigation/index.tsx
+++ client/src/components/Navigation/index.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Nav, Navbar, NavbarBrand } from 'reactstrap';
+import { Button, Container, Nav, Navbar, NavbarBrand, NavbarText } from 'reactstrap';
+
+import UserContext, { initialUserState } from '../../contexts/user';
 
 function Navigation() {
+  const userContext = useContext(UserContext);
+  const { user } = userContext.userState;
+
+  const logout = () => {
+    userContext.userDispatch({ type: 'logout', payload: initialUserState });
+  };
+
   return (
     <Navbar color="light" light sticky="top" expand="md">
       <Container>
@@ -10,6 +19,26 @@ function Navigation() {
           📝
         </NavbarBrand>
         <Nav className="mr-auto" />
+        {user._id === '' ? (
+          <div>
+            <NavbarText tag={Link} to="/login">
+              Login
+            </NavbarText>
+            <NavbarText className="mx-2">|</NavbarText>
+            <NavbarText tag={Link} to="/register">
+              Sign Up
+            </NavbarText>
+          </div>
+        ) : (
+          <div>
+            <Button outline tag={Link} to="/edit">
+              Post a Blog
+            </Button>
+            <Button onClick={() => logout()} className="ml-2">
+              Logout
+            </Button>
+          </div>
+        )}
       </Container>
     </Navbar>
   );
