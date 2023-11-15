fc7242d9cf5ab5bd02a6a65b31fbf05733c83f36
mirza-sync <mirzasyahmi123@gmail.com>
2023-11-15 23:44:59 +0800

    Add auth user & validate token function

--- client/src/modules/auth.ts
+++ client/src/modules/auth.ts
@@ -1,6 +1,10 @@
+import axios from 'axios';
 import { AuthProvider, signInWithPopup, UserCredential } from 'firebase/auth';
 
+import config from '../config/config';
 import { auth } from '../config/firebase';
+import logging from '../config/logging';
+import IUser from '../interfaces/user';
 
 export const signInWithSocialMedia = (provider: AuthProvider) => {
   return new Promise<UserCredential>((resolve, reject) => {
@@ -8,4 +12,56 @@ export const signInWithSocialMedia = (provider: AuthProvider) => {
       .then((result) => resolve(result))
       .catch((error) => reject(error));
   });
+};
+
+export const authenticate = async (
+  uid: string,
+  name: string,
+  fireToken: string,
+  callback: (error: string | null, user: IUser | null) => void,
+) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: `${config.server.url}/users/login`,
+      headers: { Authorization: `Bearer ${fireToken}` },
+      data: {
+        uid,
+        name,
+      },
+    });
+    if (response.status === 200 || response.status === 201 || response.status === 304) {
+      logging.info('Authentication successful');
+      callback(null, response.data.user);
+    } else {
+      logging.warn('Unable to aunthenticate');
+      callback('Unable to authenticate', null);
+    }
+  } catch (error) {
+    logging.error(error);
+    callback(`Something went wrong. ${error}`, null);
+  }
+};
+
+export const validate = async (
+  fireToken: string,
+  callback: (error: string | null, user: IUser | null) => void,
+) => {
+  try {
+    const response = await axios({
+      method: 'GET',
+      url: `${config.server.url}/users/validate`,
+      headers: { Authorization: `Bearer ${fireToken}` },
+    });
+    if (response.status === 200 || response.status === 304) {
+      logging.info('Token validation successful');
+      callback(null, response.data.user);
+    } else {
+      logging.warn('Token invalid');
+      callback('Unable to validate user', null);
+    }
+  } catch (error) {
+    logging.error(error);
+    callback(`Something went wrong. ${error}`, null);
+  }
 };
